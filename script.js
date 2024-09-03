a6781a888c3c7129ababc6684a37d040db88bd80
Rusydina Novitasari <rnvitas697@gmail.com>
2024-09-03 20:36:17 +0700

    commit keempat

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 //harus number
 //validasi value
 
-function cariArray(arry){
+function cariArray(number){
+    const number =""
     const arry =""
     const start=arry[0];
 
-    for(let i = 0; i)
+    for(let i = 0; i< arry.length; i++){
+        if(arry[i] = number){
+            return number;
+        }else if (arry[i] != number ){
+            return "inputan harus number"
+        }else if (number )
+    } 
     
 }
