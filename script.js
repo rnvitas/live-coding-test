b1a7ac41b10c67088abbee7d99487511c92711c6
Rusydina Novitasari <rnvitas697@gmail.com>
2024-09-03 20:48:06 +0700

    commit keenam

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 //harus number
 //validasi value
 
-function cariArray(number) {
-  const number = "";
+function cariArray() {
+  //   const number = "";
   const arry = [];
   //   const start = arry[0];
 
   for (let i = 0; i < arry.length; i++) {
     if (isNumber(number)) {
-      if ((arry[i] = number)) {
+      if (arry[i] == number) {
         return number;
-      } else if (arry[i] != number) {
-        return "inputan harus number";
+      } else if (arry[i] !== number) {
+        return "data tidak ada";
       }
     } else {
       console.log("data harus number");
@@ -22,6 +22,6 @@ function cariArray(number) {
 }
 
 const arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const result = cariArray(11);
+const result = cariArray();
 
-console.log(result); // Output: [2, 4, 6, 8, 10]
+console.log(result);
